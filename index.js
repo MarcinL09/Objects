45e66db7b91359500d4a26c8edd4d85c35441f43
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-10-06 19:40:34 +0200

    added 'createUser' function

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function getFullNameFromUser(user) {
 
 console.log(getFullNameFromUser(user));
 
+// Write the createUser function
+
+
+user.firstName = 'Katie',
+user.lastName = 'Williams'
+
+function createUser(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const userName = createUser(user);
+console.log(userName);
+
