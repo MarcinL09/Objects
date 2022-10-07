2c6a584827780d32f6390864fa060f93b18ca514
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-10-07 07:35:34 +0200

    added checkIfPropertyIsDefined function

--- index.js
+++ index.js
@@ -16,12 +16,29 @@ console.log(getFullNameFromUser(user));
 user.firstName = 'Katie',
 user.lastName = 'Williams'
 
-function createUser(user) {
-    return user.firstName + ' ' + user.lastName;
+function createUser(firstName, lastName) {
+    return {
+        fullName: firstName + ' ' + lastName,
+        firstName,
+        lastName
+    }
 }
 
-const userName = createUser(user);
+const userName = createUser(user.firstName, user.lastName);
 console.log(userName);
+
+// write the checkIfPropertyIsDefined function
+const userObject = {
+    height: 170
+}
+function checkIfPropertyIsDefined(userObject, propertyName) {
+    return userObject[propertyName] === undefined;
+}
+
+const checkProperty = checkIfPropertyIsDefined(userObject, 'firstName'); // true
+const checkPropertysecond = checkIfPropertyIsDefined(userObject, 'height'); // false
+console.log(checkProperty);
+console.log(checkPropertysecond)
 
 // Add up all of the salaries
 
@@ -33,4 +50,25 @@ const salaries = {
 
 const salarySum = salaries.john + salaries.adam + salaries.katie;
 console.log(salarySum);
+
+// Write the isObject function. Use the typeof operator.
+
+// function isObject(object) {
+//     return;
+// }
+//
+// isObject({}); // true
+// isObject(null); // false
+
+// Write the removeProperty function. It should return true if there is a property with a given key to
+// remove. Otherwise, it should return false.
+//     const user = {
+//     name: 'John'
+// }
+// removeProperty(user, 'name'); // true
+// removeProperty(user, 'name'); // false
+// removeProperty(user, 'weight'); // false
+// console.log(user); // {}
+
+
 
