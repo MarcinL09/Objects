3f124f4906c4ba6e6c47b7abce848d13d4516ea2
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-10-08 20:16:38 +0200

    added Grasshopper - Object syntax debug task

--- index.js
+++ index.js
@@ -175,3 +175,22 @@ function animalDescription(animalObject){
     return (`This ${animalObject.color} ${animalObject.name} has ${animalObject.legs} legs.`);
 }
 console.log(animalDescription(animal));
+
+// Grasshopper - Object syntax debug
+
+const rooms = {
+    first: {
+        description: 'This is the first room',
+        items: {
+            chair: 'The old chair looks comfortable',
+            lamp: 'This lamp looks ancient'
+        }
+    },
+    second: {
+        description: 'This is the second room',
+        items: {
+            couch: 'This couch looks like it would hurt your back',
+            table: 'On the table there is an unopened bottle of water'
+        }
+    }
+}
